1a673e24856e9ea0087211a5697841ebc1e9f611
unknown <tyler.r.roll@gmail.com>
2021-09-07 17:11:46 -0600

    handlebars

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
-
+app.use(express.static(path.join(__dirname, 'public')))
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
